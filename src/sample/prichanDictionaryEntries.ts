c9393067f22148b583bcf5fee90f1b6e5d2ae8dc
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-12 03:42:24 +0900

    サンプル用のデータを追加

---
+++ src/sample/prichanDictionaryEntries.ts
@@ -0,0 +1,74 @@
+export let prichanDictionaryEntries = [
+    {
+        reading: "れでぃーあくしょん",
+        word: "レディー・アクション！",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "わんつーすいーつ",
+        word: "ワン・ツー・スウィーツ",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "すきすきせんさー",
+        word: "スキスキセンサー",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "きらりかくせいりいんかーねーしょん",
+        word: "キラリ覚醒☆リインカーネーション",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "すーぱーきゅーてぃーすーぱーがーる",
+        word: "SUPER CUTIE SUPER GIRL",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "おとめあてんしょんぷりーず",
+        word: "乙女アテンションプリーズ",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "ぷれいさうんど",
+        word: "Play Sound☆",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "こめてぃっくしるえっと",
+        word: "COMETIC SILHOUETTE",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "ねてもさめてもどりーみんがーる",
+        word: "寝ても覚めてもDREAMIN' GIRL",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "きらっとすたーと",
+        word: "キラッとスタート",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "ふぉーちゅんからっと",
+        word: "フォーチュン・カラット",
+        category: "固有名詞",
+        comment: "",
+    },
+    {
+        reading: "ぱーふぇくとふぃなーれ",
+        word: "パーフェクト・フィナーレ",
+        category: "固有名詞",
+        comment: "",
+    },
+];
