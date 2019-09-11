180cc85bedfef3a88baf429f499c9078422db3de
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-12 03:41:32 +0900

    単語の入力バーを実装

---
+++ src/components/dictionaryInput.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="dictionary-input">
+        <dictionary-row class="dictionary-row" :editable="true" :entry="entry"></dictionary-row>
+        <div class="dictionary-input-tools">
+            <button type="button" @click="clear">クリア</button>
+            <button type="button" @click="$emit('send')">送信</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import DictionaryRow from "@components/dictionaryRow";
+    import Vue from "vue";
+
+    export default {
+        name: "dictionaryInput",
+        components: {DictionaryRow},
+        props: ["entry"],
+        methods: {
+            clear: function () {
+                Vue.set(this.entry, "reading", "");
+                Vue.set(this.entry, "word", "");
+                Vue.set(this.entry, "category", "");
+                Vue.set(this.entry, "comment", "");
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .dictionary-input {
+        .dictionary-row {
+            height: 30px;
+            margin-bottom: 15px;
+        }
+
+        .dictionary-input-tools {
+            display: flex;
+            height: 30px;
+
+            & > button {
+                margin-right: 30px;
+
+                &:first-of-type {
+                    margin-left: auto;
+                }
+            }
+        }
+    }
+</style>
