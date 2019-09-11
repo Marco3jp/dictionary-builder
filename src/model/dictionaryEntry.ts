408506d1fa302004a10b2f446495ef4a75b8caed
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-12 03:39:26 +0900

    単語のモデルを追加

---
+++ src/model/dictionaryEntry.ts
@@ -0,0 +1,9 @@
+export interface DictionaryEntry {
+    reading?: string,
+    word?: string,
+    category?: string,
+    comment?: string,
+    created_at?: number,
+    updated_at?: number,
+    deleted_at?: number,
+}
