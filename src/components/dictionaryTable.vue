a84f8f6f9fe431228d05f3543589dc1ff2fd0c88
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-12 03:41:04 +0900

    複数の単語を表示するテーブルを実装

---
+++ src/components/dictionaryTable.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="dictionary-table">
+        <div class="dictionary-table-header">
+            <div class="dictionary-table-header-reading">
+                reading
+            </div>
+            <div class="dictionary-table-header-word">
+                word
+            </div>
+            <div class="dictionary-table-header-category">
+                category
+            </div>
+            <div class="dictionary-table-header-created">
+                created
+            </div>
+            <div class="dictionary-table-header-updated">
+                updated
+            </div>
+        </div>
+        <dictionary-row class="dictionary-row" v-for="(entry, index) in dictionaryEntries" :entry="entry"
+                        :key="index"></dictionary-row>
+    </div>
+</template>
+
+<script lang="ts">
+    import DictionaryRow from "@components/dictionaryRow"
+
+    export default {
+        name: "dictionaryTable",
+        components: {DictionaryRow},
+        props: ["dictionaryEntries"],
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "../_values.scss";
+
+    .dictionary-table-header {
+        display: flex;
+        width: 100%;
+
+        & > div {
+            text-align: center;
+            width: $table-cell-width;
+        }
+    }
+
+    .dictionary-row {
+        height: 30px;
+        margin-bottom: 15px;
+    }
+</style>
