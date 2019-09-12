82221bbb8082151d4ff0d106b78b7fe4b481bc97
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-12 19:22:05 +0900

    ダウンロードボタンの実装

--- src/main.vue
+++ src/main.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="dictionary-builder">
-        <h1>Dictionary builder</h1>
-        <p>Google日本語入力やMozcで使える辞書を作って共有しよう！</p>
+        <div class="flex">
+            <div id="description">
+                <h1>Dictionary builder</h1>
+                <p>Google日本語入力やMozcで使える辞書を作って共有しよう！</p>
+            </div>
+            <div id="dictionary-downloader">
+                <a ref="dictionaryDownloadLink" id="dictionary-downloader-hidden-link" download="" href=""></a>
+                <button id="dictionary-downloader-button" type="button" @click="download">ダウンロード</button>
+            </div>
+        </div>
         <dictionary-input class="dictionary-input" :entry="creatingDictionaryEntry"></dictionary-input>
         <dictionary-table class="dictionary-table" :dictionaryEntries="createdDictionaryEntries"></dictionary-table>
     </div>
@@ -32,6 +40,18 @@
         },
         mounted: function () {
             this.createdDictionaryEntries = prichanDictionaryEntries;
+        },
+        methods: {
+            download() {
+                let dictionaryString = "";
+                this.createdDictionaryEntries.forEach(entry => {
+                    dictionaryString += `${entry.reading}\t${entry.word}\t${entry.category}\t${entry.comment}\n`
+                });
+                this.$refs.dictionaryDownloadLink.href = URL.createObjectURL(new Blob([dictionaryString], {"type": "text/plain"}));
+                // @ts-ignore
+                this.$refs.dictionaryDownloadLink.download = `${window.config.dictionaryBaseName}_${(new Date(Date.now())).toISOString()}.txt`;
+                this.$refs.dictionaryDownloadLink.click();
+            },
         }
     }
 </script>
@@ -43,6 +63,20 @@
     }
 </style>
 <style scoped lang="scss">
+    .flex {
+        display: flex;
+
+        #description {
+            flex-grow: 1;
+        }
+
+        #dictionary-downloader {
+            #dictionary-downloader-hidden-link {
+                display: none;
+            }
+        }
+    }
+
     .dictionary-builder {
         width: 1400px;
         margin: 80px auto;
