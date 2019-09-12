68f50170ba58ac5f78e25041726c749590850050
marco <28888207+Marco3jp@users.noreply.github.com>
2019-09-12 19:26:17 +0900

    送信処理と受信処理を実装

--- src/main.vue
+++ src/main.vue
@@ -10,15 +10,17 @@
                 <button id="dictionary-downloader-button" type="button" @click="download">ダウンロード</button>
             </div>
         </div>
-        <dictionary-input class="dictionary-input" :entry="creatingDictionaryEntry"></dictionary-input>
+        <dictionary-input class="dictionary-input" :entry="creatingDictionaryEntry" @post="postData"></dictionary-input>
         <dictionary-table class="dictionary-table" :dictionaryEntries="createdDictionaryEntries"></dictionary-table>
     </div>
 </template>
 
 <script lang="ts">
+    import * as firebase from "firebase/app";
+    import 'firebase/firestore';
     import DictionaryTable from "@components/dictionaryTable";
     import DictionaryInput from "@components/dictionaryInput";
-    import {prichanDictionaryEntries} from "./sample/prichanDictionaryEntries";
+    import {DictionaryEntry} from "./model/dictionaryEntry";
 
     export default {
         name: "Main",
@@ -26,7 +28,7 @@
         data() {
             return {
                 db: undefined,
-                createdDictionaryEntries: undefined,
+                createdDictionaryEntries: [],
                 creatingDictionaryEntry: {
                     reading: "",
                     word: "",
@@ -39,7 +41,17 @@
             }
         },
         mounted: function () {
-            this.createdDictionaryEntries = prichanDictionaryEntries;
+            // this.createdDictionaryEntries = prichanDictionaryEntries;
+            let test = firebase.initializeApp({
+                // @ts-ignore
+                apiKey: window.config.apiKey,
+                // @ts-ignore
+                authDomain: window.config.authDomain,
+                // @ts-ignore
+                projectId: window.config.projectId
+            });
+            this.db = firebase.firestore();
+            this.getData();
         },
         methods: {
             download() {
@@ -52,6 +64,36 @@
                 this.$refs.dictionaryDownloadLink.download = `${window.config.dictionaryBaseName}_${(new Date(Date.now())).toISOString()}.txt`;
                 this.$refs.dictionaryDownloadLink.click();
             },
+            postData() {
+                if (this.isValidatedEntry(this.creatingDictionaryEntry)) {
+                    this.creatingDictionaryEntry.created_at = firebase.firestore.FieldValue.serverTimestamp();
+                    this.creatingDictionaryEntry.updated_at = firebase.firestore.FieldValue.serverTimestamp();
+                    this.creatingDictionaryEntry.deleted_at = 0;
+                    this.db.collection("entries").add(this.creatingDictionaryEntry).then(result => {
+                        console.log(result);
+                    }).catch(e => {
+                        console.warn(e);
+                    });
+                }
+            },
+            getData() {
+                this.db.collection("entries").get().then((entries) => {
+                    entries.forEach(entry => {
+                        this.createdDictionaryEntries.push(entry.data());
+                    })
+                });
+            },
+            // TODO: Readingに細かなバリデーションを用意
+            isValidatedEntry(entry: DictionaryEntry) {
+                return typeof entry !== "undefined"
+                    && typeof entry.reading !== "undefined"
+                    && entry.reading !== ""
+                    && typeof entry.word !== "undefined"
+                    && entry.word !== ""
+                    && typeof entry.created_at === "undefined"
+                    && typeof entry.updated_at === "undefined"
+                    && typeof entry.deleted_at === "undefined"
+            }
         }
     }
 </script>
